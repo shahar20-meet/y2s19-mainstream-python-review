93936fe8bf1f5d9f31fb9856513c959f0087b283
myname21-meet <myname21@meet.mit.edu>
2019-07-28 09:13:39 -0400

    idk again

--- part1.py
+++ part1.py
@@ -1,13 +1,25 @@
 # Part 1 of the Python Review lab.
 
 def hello_world():
-	pass
+	print("hello world")
+
+hello_world()
 
 def greet_by_name(name):
-	pass
+	
+	print('Hello, ' + name)
+
+name = input("what is your name? :")
+print(name)
+greet_by_name(name)
 
 def encode(x):
-	pass
+	
+	return ( int(x) + 3953531)
+x = input("enter your number: ")
+encode(x)
 
 def decode(y):
-	pass
+	return (int(y) - 39531)
+
+print(decode(29582039402))
--- part2.py
+++ part2.py
@@ -1,7 +1,10 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
-
+	if 1<y<250 and 500<x<1000:
+		print("num is valid")
+	else:
+		print("Invalod input: Outside range")
+encode(10,600)
 def decode(coded_message):
 	pass
